c6cf06455f2bbf791da4e88f8ad2fa26e201d291
Jo <johannes_dob@web.de>
2018-11-30 21:33:22 +0100

    lander page

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,6 +4,32 @@ class Footer extends React.Component {
     render() {
         return (
             <footer id="mlpipe-footer">
+                <div className="section">
+                    <div className="section--content">
+                        <h4>Contact</h4>
+                        <span>+49 1707777777</span>
+                        <span>some-mail@mlpipe.com</span>
+                    </div>
+                </div>
+
+                <div className="section">
+                    <div className="section--content">
+                        <h4>Company</h4>
+                        <span>About</span>
+                        <span>Contact</span>
+                        <span>Careers</span>
+                    </div>
+                </div>
+
+                <div className="section">
+                    <div className="section--content">
+                        <h4>Legal</h4>
+                        <span>Privacy Policy</span>
+                        <span>Terms of Service</span>
+                        <span>Cookies</span>
+                    </div>
+                </div>
+                <div className="copy-right">Copyright 2019</div>
             </footer>
         );
     }
--- src/components/lander/index.js
+++ src/components/lander/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Parallax } from 'react-parallax';
+import { TextField } from '@rmwc/textfield';
+import { Button } from '@rmwc/button';
 
 
 class Lander extends React.Component {
@@ -43,10 +45,11 @@ class Lander extends React.Component {
                             <h2>Focus on Results</h2>
                             <h3>and not on your toolchain</h3>
                             <div id="text">
-                                A crutial part of successfull machine learning is your toolchain. Reproducability ... Lore 
-                                ipsum Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-                                invidunt ut labore et dolore magna aliquyam  At vero eos et accusam et justo duo dolores et ea rebum.
-                                Stet clita kasd gubergren, no sea takimata sanctus est
+                                With increased useage of Machine Learning within systems the risk of technical debt 
+                                also increases and is not always visible from the start. MLPipe is helping to reduce these technical debts by
+                                addressing specific engineering topics such as data pipelining, process management, reproducibility, configuration, and
+                                training distribution & monitoring. This enables you to focus on the actual research part to improve performance
+                                while easily scale, collaborate within a team to get results faster and more efficient.
                             </div>
                         </div>
                     </div>
@@ -55,6 +58,7 @@ class Lander extends React.Component {
 
                 <Parallax
                     blur={0}
+                    className="parallax-section-wrapper"
                     bgImage={'images/hero.svg'}
                     bgImageAlt="hero"
                     strength={180}
@@ -62,12 +66,81 @@ class Lander extends React.Component {
                     <div className="parallalx-section" />
                 </Parallax>
 
-                <div className="lander-section">
-
+                <div className="lander-section description-section--reverse">
+                    <div className="card-image card-image--left">
+                        <img src="images/cloud-service.svg" alt="cloud-service" />
+                    </div>
+                    <div className="card-text">
+                        <div className="card-text--content">
+                            <h1>Cloud Training</h1>
+                            <h2>It is there when you need it</h2>
+                            <div className="text">
+                                The cloud is a perfect place for training Machine Learning models. It is not always easy 
+                                to predict the computing power needed and the needs are highly fluctuating.<br></br>
+                                Cloud computing solves both of these problems while reducing the costs.
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="lander-section" style={{background: "#eee"}}>
-                
+                <div className="lander-section description-section">
+                    <div className="card-text">
+                        <div className="card-text--content">
+                            <h1>Data Piplines</h1>
+                            <h2>Pre- and Post process your data</h2>
+                            <div className="text">
+                                Data is where most of the technical debt in Machine Learning systems is hidden.<br></br>
+                                MLPipe offers integrated, reproducable and easy to use 
+                                data piplines during training and inference.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-image card-image--right">
+                        <img src="images/data_pipline.svg" alt="cloud-service" />
+                    </div>
+                </div>
+
+
+                <div className="lander-section description-section--reverse">
+                    <div className="card-image card-image--left">
+                        <img src="images/collaboration.svg" alt="cloud-service" />
+                    </div>
+                    <div className="card-text">
+                        <div className="card-text--content">
+                            <h1>Collaboration</h1>
+                            <h2>Scale teams as you grow</h2>
+                            <div className="text">
+                                Scaling Machine Learning teams and organizations for larger projects
+                                is a challange itself.<br></br>
+                                Increase collaboration with MLPipe with the included managament for trained Models 
+                                and current resources combined with its sufficticated analysis tools to compare results.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="lander-section" style={{minHeight: "100px"}}></div>
+
+                <Parallax
+                    blur={0}
+                    className="parallax-section-wrapper-2"
+                    bgImage={'images/second_parallax.jpg'}
+                    bgImageAlt="hero"
+                    strength={180}
+                >
+                    <div className="parallalx-section-2" />
+                </Parallax>
+
+                <div className="lander-section get-in-touch">
+                    <div className="get-in-touch--content">
+                        <h1>Get in touch</h1>
+                        <h2>And boost your future</h2>
+                        <form onSubmit={(e) => {e.preventDefault(); window.alert("Not implemented yet :/");}}>
+                            <TextField className="email" label="Your Email" name="email" required={true}/>
+                            <textarea placeholder="Your Message" className="textarea"/>
+                            <Button raised className="submit">Send Message</Button>
+                        </form>
+                    </div>
                 </div>
             </div>
         ); 
