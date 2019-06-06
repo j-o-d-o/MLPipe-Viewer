58a91976df42b9192d6a2c43280d393992dacecb
Jo <jdobler@protonmail.com>
2019-06-06 13:09:53 +0200

    comment out server page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -52,9 +52,11 @@ class Header extends React.Component {
                     <Button className="nav-button">
                         <NavLink to="/job" activeClassName="active-route">Jobs</NavLink>
                     </Button>
+                    {/*
                     <Button className="nav-button">
                         <NavLink to="/servers" activeClassName="active-route">Servers</NavLink>
                     </Button>
+                    */}
                     <Button className="nav-button">
                         <NavLink exact to={"/user/" + loggedUser._id} activeClassName="active-route">Profile</NavLink>
                     </Button>
@@ -76,10 +78,12 @@ class Header extends React.Component {
                         <NavLink exact to="/job" activeClassName="active-route"
                             onClick={() => this.setState({sideMenuOpen: false})}>Jobs</NavLink>
                     </ListItem>
+                    {/*
                     <ListItem>
                         <NavLink exact to="/servers" activeClassName="active-route"
                             onClick={() => this.setState({sideMenuOpen: false})}>Servers</NavLink>
                     </ListItem>
+                    */}
                     <ListItem>
                         <NavLink exact to={"/user/" + loggedUser._id} activeClassName="active-route"
                             onClick={() => this.setState({sideMenuOpen: false})}>Profile</NavLink>
